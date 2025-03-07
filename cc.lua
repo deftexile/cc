17ed9c45806886519bdaa46621df04f258871a63
Zak Tirone <48061602+deftexile@users.noreply.github.com>
2025-03-06 21:23:02 -0600

    Update cc.lua

--- cc.lua
+++ cc.lua
@@ -1,1 +1,4 @@
-print("Hello")
+print("Running Program VIA Github...")
+local chatBox = peripheral.find("chatBox")
+
+chatBox.sendToastToPlayer("Hehe it worked...", "Hello", "CetiOmicron", "&4&lTesting!", "()", "&c&l")
